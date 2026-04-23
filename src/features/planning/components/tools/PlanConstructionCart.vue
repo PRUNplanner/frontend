2c0dd3ad112ca6d3e1d38de8887111cc9039ed20
Rich <rich@richjenks.com>
2026-04-23 12:52:22 +0100

    Fix false positive

--- src/features/planning/components/tools/PlanConstructionCart.vue
+++ src/features/planning/components/tools/PlanConstructionCart.vue
@@ -129,12 +129,18 @@
 			.sort((a, b) => a.localeCompare(b));
 	});
 
+	function getTotalBuildingAmount(ticker: string): number {
+		const builtAmount = constructedMap?.get(ticker) ?? 0;
+		const plannedAmount = localBuildingAmount.value[ticker] ?? 0;
+		return builtAmount + plannedAmount;
+	}
+
 	const deficitWorkforceTypes = computed(() => {
 		return (workforceTypeNames as WORKFORCE_TYPE[]).filter(
 			(workforceType) =>
 				buildingTicker.value.reduce((sum, ticker) => {
 					const building = buildingsMap.value[ticker];
-					const amount = localBuildingAmount.value[ticker] ?? 0;
+					const amount = getTotalBuildingAmount(ticker);
 					const field = `${workforceType}s` as keyof NonNullable<
 						IBuilding["habitations"]
 					>;
@@ -142,7 +148,7 @@
 				}, 0) >
 				buildingTicker.value.reduce((sum, ticker) => {
 					const building = buildingsMap.value[ticker];
-					const amount = localBuildingAmount.value[ticker] ?? 0;
+					const amount = getTotalBuildingAmount(ticker);
 					const field = `${workforceType}s` as keyof NonNullable<
 						IBuilding["habitations"]
 					>;
