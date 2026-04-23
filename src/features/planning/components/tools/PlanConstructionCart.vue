1265a31d38430a2f6c49919c0e991397fd7ea1c2
Rich <rich@richjenks.com>
2026-04-23 13:14:37 +0100

    Added warning tooltip

--- src/features/planning/components/tools/PlanConstructionCart.vue
+++ src/features/planning/components/tools/PlanConstructionCart.vue
@@ -354,7 +354,17 @@
 				<tr>
 					<th>Building</th>
 					<th v-if="constructedMap">Built</th>
-					<th>Amount</th>
+					<th class="inline-flex">
+						Amount
+							<PTooltip v-if="deficitWorkforceTypes.length > 0">
+							<template #trigger>
+								<PIcon class="text-amber-400 ml-1 relative top-px">
+									<WarningAmberRound />
+								</PIcon>
+							</template>
+							Insufficient habitation will reduce production efficiency
+						</PTooltip>
+					</th>
 					<th>Planned</th>
 					<th
 						v-for="mat in uniqueMaterials"
