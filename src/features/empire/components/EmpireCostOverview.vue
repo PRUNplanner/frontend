89d8c9296e25a2563124b73f7de0ef20bc022411
Lumivient <lumivient@proton.me>
2026-09-06 09:07:23 +0100

    Reorder empire cost overview stats

--- src/features/empire/components/EmpireCostOverview.vue
+++ src/features/empire/components/EmpireCostOverview.vue
@@ -25,21 +25,6 @@
 	<div
 		class="grid grid-cols-[auto_auto] grid-rows-2 sm:grid-rows-1 sm:grid-cols-[1fr_auto_auto_auto_auto_1fr] gap-6 child:child:text-center">
 		<div class="sm:col-2">
-			<div class="text-white/40 text-xs">{{ $t("terms.profit") }}</div>
-			<div class="text-white text-xl">
-				{{ formatNumber(costOverview.totalProfit) }}
-			</div>
-			<div class="text-white/40 text-xs">
-				{{
-					formatNumber(
-						(costOverview.totalProfit / costOverview.totalRevenue) *
-							100
-					)
-				}}
-				%
-			</div>
-		</div>
-		<div>
 			<div class="text-white/40 text-xs">{{ $t("terms.revenue") }}</div>
 			<div class="text-white text-xl">
 				{{ formatNumber(costOverview.totalRevenue) }}
@@ -54,6 +39,21 @@
 				{{
 					formatNumber(
 						(costOverview.totalCost / costOverview.totalRevenue) *
+							100
+					)
+				}}
+				%
+			</div>
+		</div>
+		<div>
+			<div class="text-white/40 text-xs">{{ $t("terms.profit") }}</div>
+			<div class="text-white text-xl">
+				{{ formatNumber(costOverview.totalProfit) }}
+			</div>
+			<div class="text-white/40 text-xs">
+				{{
+					formatNumber(
+						(costOverview.totalProfit / costOverview.totalRevenue) *
 							100
 					)
 				}}
